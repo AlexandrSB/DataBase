b21b3f32546369c9fa04bcdcfdc6f3a66ee22b7a
SB <Finn1435@rambler.ru>
2023-05-29 17:06:54 +0800

    add bootstrap (work)

---
+++ src/main/java/com/example/restservice/data/repos/AttributeIntegerValueRepo.java
@@ -0,0 +1,7 @@
+package com.example.restservice.data.repos;
+
+import com.example.restservice.data.domain.AttributeIntegerValue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttributeIntegerValueRepo extends CrudRepository<AttributeIntegerValue, Long> {
+}
---
+++ src/main/java/com/example/restservice/data/repos/AttributeStringValueRepo.java
@@ -0,0 +1,7 @@
+package com.example.restservice.data.repos;
+
+import com.example.restservice.data.domain.AttributeStringValue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttributeStringValueRepo extends CrudRepository<AttributeStringValue, Long> {
+}
