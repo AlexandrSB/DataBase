6950d81502cd835cadb2a9b8074c9b821ffae02e
SB <Finn1435@rambler.ru>
2023-08-06 23:06:22 +0800

    adding second database (working)

--- src/main/java/com/example/restservice/equipmentData/equipmentRepos/AttributeValueRepo.java
+++ src/main/java/com/example/restservice/equipmentData/equipmentRepos/AttributeValueRepo.java
@@ -2,8 +2,6 @@ package com.example.restservice.equipmentData.equipmentRepos;
 
 import com.example.restservice.equipmentData.equipmentDomain.AttributeValue;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AttributeValueRepo extends CrudRepository<AttributeValue, Long> {
 }
